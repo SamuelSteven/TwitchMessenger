92322229e432eae4429612e1b7574cc9dba82561
SamuelSteven <samuel.steven880@gmail.com>
2021-12-29 15:56:16 +0700

    Creating the chat UI

--- src/App.js
+++ src/App.js
@@ -1,38 +1,72 @@
+import React, { useEffect, useState } from 'react';
 import { StreamChat } from 'stream-chat';
 import {
   Chat,
   Channel,
+  ChannelHeader,
   ChannelList,
   MessageList,
   MessageInput,
   Thread,
   Window,
 } from 'stream-chat-react';
+import '@stream-io/stream-chat-css/dist/css/index.css';
+
+const filters = { type: 'messaging' };
+const options = { state: true, presence: true, limit: 10 };
+const sort = { last_message_at: -1 };
 
 const client = StreamChat.getInstance('65ukcf2g2k62');
 
-client.connectUser(
-  {
-    id: 'dave-matthews',
-    name: 'Dave Matthews',
-  },
-  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZGF2ZS1tYXR0aGV3cyJ9.ZNRqlbFydU8c7FEi95Zi3RFZ1ag0pW - JMtsz_c6vZcg,
-);
+const App = () => {
+  const [clientReady, setClientReady] = useState(false);
+  const [channel, setChannel] = useState(null);
 
-const channel = client.channel('gaming', {
-  name: 'Gaming Channel',
-  members: ['dave-matthews', 'trey-anastasio'],
-});
+  useEffect(() => {
+    const setupClient = async () => {
+      try {
+        await client.connectUser(
+          {
+            id: 'dave-matthews',
+            name: 'Dave Matthews',
+          },
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZGF2ZS1tYXR0aGV3cyJ9.ZNRqlbFydU8c7FEi95Zi3RFZ1ag0pW-JMtsz_c6vZcg',
+        );
+        const channel = await client.channel('gaming', 'gaming-demo', {
+          name: 'Gaming Demo',
+        })
+        setChannel(channel)
 
-const App = () => (
-  <Chat client={client}>
-    <Channel channel={channel}>
-      <Window>
-        <ChannelHeader />
-        <MessageList />
-        <MessageInput />
-      </Window>
-      <Thread />
-    </Channel>
-  </Chat>
-);
+        setClientReady(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setupClient();
+  }, []);
+
+  if (!clientReady) return null;
+  const customStyle = {
+    '--primary-color': 'green',
+    '--md-font': '1.2rem',
+    '--xs-m': '1.2rem',
+    '--xs-p': '1.2rem',
+  };
+
+  return (
+    <Chat client={client} customStyles={customStyle}>
+      <ChannelList filters={filters} sort={sort} options={options} />
+      <Channel channel={channel}>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  );
+};
+
+export default App;
