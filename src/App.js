5f835ba25788f59c5d833fb05677fb286c3016a4
SamuelSteven <samuel.steven880@gmail.com>
2021-12-29 14:31:08 +0700

    Creating a chat client, channel, and connecting dummy user

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { StreamChat } from 'stream-chat';
+import {
+  Chat,
+  Channel,
+  ChannelList,
+  MessageList,
+  MessageInput,
+  Thread,
+  Window,
+} from 'stream-chat-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const client = StreamChat.getInstance('65ukcf2g2k62');
 
-export default App;
+client.connectUser(
+  {
+    id: 'dave-matthews',
+    name: 'Dave Matthews',
+  },
+  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZGF2ZS1tYXR0aGV3cyJ9.ZNRqlbFydU8c7FEi95Zi3RFZ1ag0pW - JMtsz_c6vZcg,
+);
+
+const channel = client.channel('gaming', {
+  name: 'Gaming Channel',
+  members: ['dave-matthews', 'trey-anastasio'],
+});
+
+const App = () => (
+  <Chat client={client}>
+    <Channel channel={channel}>
+      <Window>
+        <ChannelHeader />
+        <MessageList />
+        <MessageInput />
+      </Window>
+      <Thread />
+    </Channel>
+  </Chat>
+);
